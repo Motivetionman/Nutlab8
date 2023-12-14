65cff158d5f1c93d1a40fc91d1c6667228317426
Motivationman_74 <120492095+Motivetionman@users.noreply.github.com>
2023-12-15 01:14:36 +0700

    Update lab8_3.cpp

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,63 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int year;
+int height;
+int bounty;
+
+int main()
+{
+    cout << "Enter your age: ";
+    cin >> year;
+
+    if (year <= 25)
+    {
+        cout << "Enter your height: ";
+        cin >> height;
+
+        if (height < 100)
+        {
+            cout << "Your character = Chopper";
+        }
+        else if (height < 180)
+        {
+            cout << "Your character = Usopp";
+        }
+
+        else if (height >= 180)
+        {
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+        }
+
+        if (bounty > 1100000000)
+        {
+            cout << "Your character = Zoro";
+        }
+        else if (bounty <= 1100000000)
+        {
+            cout << "Your character = Sanji";
+        }
+    }
+    else if (year <= 60)
+    {
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+
+        if (bounty > 500000000)
+        {
+            cout << "Your character = Jinbe";
+        }
+        else if (bounty <= 500000000)
+        {
+            cout << "Your character = Franky";
+        }
+    }
+    else
+    {
+        cout << "Your character = Brook";
+    }
+
+    return 0;
+}
